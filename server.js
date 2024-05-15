0d154e188105ad5f1d07d0822c549817f1687d97
Hvk26 <hvk8789@gmail.com>
2024-05-15 14:28:08 +0530

    My First Commit

---
+++ server.js
@@ -0,0 +1,64 @@
+// initialising import sunctions and variables
+
+const express = require('express');
+const app = express();
+const PORT = 8000;
+require('dotenv').config()
+
+// response data array
+let pokemon = {
+
+    "pikachu":{
+        name:"Pikachu",
+        type: "Lightining",
+        power: "76xp",
+        agility:"90ap"
+    },
+    "bulbasaur":{
+        name:"Bulbasaur",
+        type: "Forest",
+        power: "67xp",
+        agility:"50ap"
+    },
+    "charizard":{
+        name:"Charizard",
+        type: "Fire",
+        power: "89xp",
+        agility:"77ap"
+    },
+    "Blastoise":{
+        name:"Blastoise",
+        type: "Water",
+        power: "85xp",
+        agility:"70ap"
+    },
+    "no name":{
+        name:"unknown",
+        type: "unknown",
+        power: "unknown",
+        agility:"unknown"
+    }
+}
+
+// server side request and response
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/api/:name',(req,res)=>{
+    const pokemoname = req.params.name.toLowerCase();
+
+    if(pokemon[pokemoname]){
+        res.json(pokemon[pokemoname])
+    }
+    else{
+        res.json(pokemon['no name'])
+    }
+});
+
+// server side listening
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log("server is listening")
+});
